eaaa4f24faa6cdb0124d67e31d8cca1a16c9dc9d
NguyenShinky <86399542+NguyenShinky@users.noreply.github.com>
2021-06-24 13:39:29 +0700

    Initial commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,130 @@
+﻿using lab3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab3.Controllers
+{
+    public class BookController : Controller
+    {
+
+        public ActionResult ListBook()
+        {
+            BookManagerContext context = new BookManagerContext();
+            var listBook = context.Book.ToList();
+            return View(listBook);
+        }
+
+        [Authorize]
+
+        public ActionResult Buy(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Book.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [Authorize]
+
+        public ActionResult Create()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Book book)
+        {
+            BookManagerContext context = new BookManagerContext();
+            var search = context.Book.SingleOrDefault(p => p.ID == book.ID);
+            if (search != null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Book.AddOrUpdate(book);
+                context.SaveChanges();
+                return RedirectToAction("ListBook");
+            }
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Book.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [Authorize]
+        [HttpPost]
+
+        public ActionResult Edit(Book book)
+        {
+
+            BookManagerContext context = new BookManagerContext();
+            Book bookUpdate = context.Book.SingleOrDefault(p => p.ID == book.ID);
+            if (bookUpdate != null)
+            {
+                context.Book.AddOrUpdate(book);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("LitsBook");
+
+        }
+
+
+
+        public ActionResult Delete(int Id)
+        {
+            using (BookManagerContext context = new BookManagerContext())
+            {
+                return View(context.Book.Where(p => p.ID == Id).FirstOrDefault());
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Delete(int Id, FormCollection collection)
+        {
+            try
+            {
+                using (BookManagerContext context = new BookManagerContext())
+                {
+                    Book book = context.Book.Where(p => p.ID == Id).FirstOrDefault();
+                    context.Book.Remove(book);
+                    context.SaveChanges();
+                }
+                return RedirectToAction("ListBook");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,41 @@
+﻿namespace lab3.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        [Display(Name = "Ma Sach")]
+        [Required(ErrorMessage = "Id khong duoc trong")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Ten sach khong duoc trong")]
+        [StringLength(100, ErrorMessage = "ten sach khong duoc qua 100 ky tu")]
+        [Display(Name = "Ten sach")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+
+        [Required(ErrorMessage = "Tac gia khong duoc qua 30")]
+        [StringLength(30, ErrorMessage = "Tieu de khong duoc qua 30 ky tu")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "noi dung khong duoc trong")]
+        [StringLength(255)]
+        public string Images { get; set; }
+
+        [Required(ErrorMessage = "Gia khong duoc trong")]
+        [Range(1000, 1000000, ErrorMessage = "Giá sách từ 1000- 1000000")]
+        [Display(Name = "Gia sach")]
+        public int Price { get; set; }
+    }
+}
